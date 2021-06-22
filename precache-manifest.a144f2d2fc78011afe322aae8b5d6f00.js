1a93adb4c42dddfaea15cbdef90efb7712c8c7ef
mhumza37dev <mhumza37@gmail.com>
2021-06-22 22:45:16 +0500

    Updates

---
+++ precache-manifest.a144f2d2fc78011afe322aae8b5d6f00.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f9a5385253b871bf40f5d8f8ec6823",
+    "url": "/QuranAcdemy-admin-panel/index.html"
+  },
+  {
+    "revision": "5c08af280ab81bc5864c",
+    "url": "/QuranAcdemy-admin-panel/static/css/2.a7695ab0.chunk.css"
+  },
+  {
+    "revision": "4c8ea4cf19065a409c04",
+    "url": "/QuranAcdemy-admin-panel/static/css/main.392c405e.chunk.css"
+  },
+  {
+    "revision": "5c08af280ab81bc5864c",
+    "url": "/QuranAcdemy-admin-panel/static/js/2.7cda0862.chunk.js"
+  },
+  {
+    "revision": "31b690875988a84844e74c3981dc7ad6",
+    "url": "/QuranAcdemy-admin-panel/static/js/2.7cda0862.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c8ea4cf19065a409c04",
+    "url": "/QuranAcdemy-admin-panel/static/js/main.051309c0.chunk.js"
+  },
+  {
+    "revision": "0518a12c851553ad0a8ca8d2e68c73d9",
+    "url": "/QuranAcdemy-admin-panel/static/js/main.051309c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea1c3f29a2e74ac963b6",
+    "url": "/QuranAcdemy-admin-panel/static/js/runtime-main.d57a5c4f.js"
+  },
+  {
+    "revision": "0c9f225e8f69c622f681cf1ed973cc3d",
+    "url": "/QuranAcdemy-admin-panel/static/media/fa-brands-400.0c9f225e.woff2"
+  },
+  {
+    "revision": "0ce1e868452204695c8ac1c70f693c2d",
+    "url": "/QuranAcdemy-admin-panel/static/media/fa-brands-400.0ce1e868.woff"
+  },
+  {
+    "revision": "5d10d00980077f67990f73b1961bc46e",
+    "url": "/QuranAcdemy-admin-panel/static/media/fa-brands-400.5d10d009.svg"
+  },
+  {
+    "revision": "d28c96cabe5302f1b2c90275cd3f4e69",
+    "url": "/QuranAcdemy-admin-panel/static/media/fa-brands-400.d28c96ca.eot"
+  },
+  {
+    "revision": "dfe5aa4344a2d8a29aec8d83fb3fb14e",
+    "url": "/QuranAcdemy-admin-panel/static/media/fa-brands-400.dfe5aa43.ttf"
+  },
+  {
+    "revision": "0232fe8b09de812c88e6cc27f57a3a88",
+    "url": "/QuranAcdemy-admin-panel/static/media/fa-regular-400.0232fe8b.eot"
+  },
+  {
+    "revision": "398be22b2308585b84da8e858e2d7b60",
+    "url": "/QuranAcdemy-admin-panel/static/media/fa-regular-400.398be22b.ttf"
+  },
+  {
+    "revision": "3cace4a04d941b5981ba32c6ce9afae1",
+    "url": "/QuranAcdemy-admin-panel/static/media/fa-regular-400.3cace4a0.woff"
+  },
+  {
+    "revision": "847712aaabbeba674afdda86d31cab17",
+    "url": "/QuranAcdemy-admin-panel/static/media/fa-regular-400.847712aa.woff2"
+  },
+  {
+    "revision": "d0fdc7c20b0b0b1aaf27327547735fa3",
+    "url": "/QuranAcdemy-admin-panel/static/media/fa-regular-400.d0fdc7c2.svg"
+  },
+  {
+    "revision": "00bb62bc33034479d5e82a0fd1c5efe3",
+    "url": "/QuranAcdemy-admin-panel/static/media/fa-solid-900.00bb62bc.eot"
+  },
+  {
+    "revision": "2306f642b31b774ba1d8b6b784539e19",
+    "url": "/QuranAcdemy-admin-panel/static/media/fa-solid-900.2306f642.svg"
+  },
+  {
+    "revision": "260d033f79f75d670fd09d8cf7c8bf8d",
+    "url": "/QuranAcdemy-admin-panel/static/media/fa-solid-900.260d033f.ttf"
+  },
+  {
+    "revision": "4bfbf7eb4b19d9ff9293eb177b6d0070",
+    "url": "/QuranAcdemy-admin-panel/static/media/fa-solid-900.4bfbf7eb.woff"
+  },
+  {
+    "revision": "9ae050d1876ac1763eb6afe4264e6d5a",
+    "url": "/QuranAcdemy-admin-panel/static/media/fa-solid-900.9ae050d1.woff2"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/QuranAcdemy-admin-panel/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/QuranAcdemy-admin-panel/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/QuranAcdemy-admin-panel/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/QuranAcdemy-admin-panel/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/QuranAcdemy-admin-panel/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "53033970a416368da35794389680266f",
+    "url": "/QuranAcdemy-admin-panel/static/media/team-1-800x800.53033970.jpg"
+  },
+  {
+    "revision": "230071328b705f8686cabd26a85ed6a5",
+    "url": "/QuranAcdemy-admin-panel/static/media/team-4-800x800.23007132.jpg"
+  }
+]);
