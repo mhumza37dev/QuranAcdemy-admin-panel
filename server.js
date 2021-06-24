1dd3d86d5f3e186459808e2945bb1d7ded4ffe57
mhumza37dev <mhumza37@gmail.com>
2021-06-24 20:59:12 +0500

    updated till 24-06-2021 teacher and student timeline added with _redirects file in public folder

--- server.js
+++ server.js
@@ -1,23 +1,11 @@
 const express = require("express");
+const path = require("path");
+const app = express();
 
-const port = process.env.PORT || 8080;
-var app = express();
+app.use(express.static(path.join(__dirname, "/build")));
 
-// List of all the files that should be served as-is
-let protected = ["transformed.js", "main.css", "favicon.ico"];
-
-app.get("*", (req, res) => {
-  let path = req.params["0"].substring(1);
-
-  if (protected.includes(path)) {
-    // Return the actual file
-    res.sendFile(`${__dirname}/build/${path}`);
-  } else {
-    // Otherwise, redirect to /build/index.html
-    res.sendFile(`${__dirname}/build/index.html`);
-  }
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/build" + "/index.html"));
 });
 
-app.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
-});
+app.listen(9000);
